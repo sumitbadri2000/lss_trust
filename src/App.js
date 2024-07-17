01808643329fecb5edf7339a743812721634da6d
sumitbadri2000 <sumitbadri2000@gmail.com>
2024-07-18 00:27:18 +0530

    add half page and responsie

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
 import HeaderNav from "./Components/Header";
+import HomeFirst from "./Components/homefir";
 function App() {
   return (
     <>
       <HeaderNav />
       <Routes>
-        {/* <Route path="/" element={<Home />} /> */}
+        <Route path="/" element={<HomeFirst />} />
       </Routes>
     </>
   );
