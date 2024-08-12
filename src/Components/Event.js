a53446ac4a2f8b42bfe6bc10dd365a5705648f9a
sumitbadri2000 <sumitbadri2000@gmail.com>
2024-08-12 13:19:43 +0530

    add changes

--- src/Components/Event.js
+++ src/Components/Event.js
@@ -9,27 +9,28 @@ import { Autoplay, Pagination, Navigation } from "swiper/modules";
 import { Box, Flex, Image, Text } from "@chakra-ui/react";
 import img1 from "../Assests/Final_Events/1.JPG";
 import img2 from "../Assests/Final_Events/2.jpeg";
-import img3 from "../Assests/Final_Events/3.jpeg";
 import img4 from "../Assests/Final_Events/4.JPG";
 import img41 from "../Assests/Final_Events/4A.JPG";
 import img42 from "../Assests/Final_Events/4B.JPG";
-import img5 from "../Assests/Final_Events/5.jpg";
 import img6 from "../Assests/Final_Events/6.JPG";
 import img61 from "../Assests/Final_Events/6A.JPG";
 import img62 from "../Assests/Final_Events/6B.JPG";
-import img63 from "../Assests/Final_Events/6C.jpeg";
-import img7 from "../Assests/Final_Events/7.jpg";
-import img8 from "../Assests/Final_Events/8.jpeg";
-import img9 from "../Assests/Final_Events/9.jpeg";
 import img10 from "../Assests/Final_Events/10.JPG";
 import img11 from "../Assests/Final_Events/11.JPG";
+import img12 from "../Assests/Final_Events/12A.JPG";
+import img121 from "../Assests/Final_Events/12B.JPG";
+import img122 from "../Assests/Final_Events/12C.JPG";
+import img13 from "../Assests/Final_Events/17B.JPG";
+import img14 from "../Assests/Final_Events/17A.JPG";
+import img15 from "../Assests/Final_Events/17.JPG";
+
 import line from "../Assests/line.png";
 import "./epilo.css";
 const images = [
-  [img1, img2, img3, img4],
-  [img41, img42, img5, img6],
-  [img61, img62, img63, img7],
-  [img8, img9, img10, img11],
+  [img1, img2, img12, img4],
+  [img41, img42, img121, img6],
+  [img61, img62, img122, img13],
+  [img10, img11, img14, img15],
 ];
 export default function Events() {
   return (
@@ -37,19 +38,22 @@ export default function Events() {
       bgGradient="linear(to-r, #b2ebcb, #ebfafa,#edfafc,#edf9fa)"
       flexDirection={"column"}
       py={6}
-      mb={20}>
+      mb={20}
+    >
       <Box width={"80%"} margin={"auto"} mb={10}>
         <Flex justifyContent={"space-between"} padding={4}>
           <Flex
             alignItems={"center"}
             justifyContent={"center"}
             width={{ base: "100%", lg: "90%" }}
-            paddingLeft={{ base: 0, lg: 20 }}>
+            paddingLeft={{ base: 0, lg: 20 }}
+          >
             <Text
               color={"#F56A02"}
               className="epilogue-bold"
               fontSize={{ base: "1.8rem", lg: "2.6rem" }}
-              fontWeight={800}>
+              fontWeight={800}
+            >
               Event Gallery
             </Text>
             <Box display={{ base: "none", lg: "flex" }} mt={2}>
@@ -60,7 +64,8 @@ export default function Events() {
             width={"10%"}
             alignItems={"center"}
             justifyContent={"flex-end"}
-            display={{ base: "none", lg: "flex" }}>
+            display={{ base: "none", lg: "flex" }}
+          >
             <Box className="swiper-pagination" />
           </Flex>
         </Flex>
@@ -78,29 +83,33 @@ export default function Events() {
             type: "bullets",
           }}
           modules={[Autoplay, Pagination, Navigation]}
-          className="mySwiper">
+          className="mySwiper"
+        >
           {images.map((slideImages, index) => (
             <SwiperSlide key={index} style={{ background: "transparent" }}>
               <Flex
                 flexDirection={{ base: "column", lg: "row" }}
-                gap={6}
-                width="100%">
+                gap={4}
+                width="100%"
+              >
                 {slideImages.map((img, imgIndex) => (
                   <Box
                     key={imgIndex}
-                    width={{ base: "100%", lg: "25%" }}
+                    width={"100%"}
                     background="#FFFFFF"
-                    border="1px solid #d9dcde"
                     rounded="md"
                     py={2}
                     px={2}
                     boxShadow="md"
-                    height={300}>
+                    height={400} 
+                    overflow="hidden"
+                  >
                     <Image
                       src={img}
                       alt={`img${imgIndex + 1}`}
-                      width="100%"
-                      height="auto"
+                      width={"100%"}
+                      height="100%"
+                      objectFit="cover" 
                     />
                   </Box>
                 ))}
